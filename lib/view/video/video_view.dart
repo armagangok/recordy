330da8ca823f416a3f9a5902875803b9ba13a2e9
Armağan Gök <1armagangok@gmail.com>
2022-07-04 11:24:20 +0300

    video view added

---
+++ lib/view/video/video_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class VideoView extends StatelessWidget {
+  const VideoView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      shrinkWrap: true,
+      children: const [],
+    );
+  }
+}
